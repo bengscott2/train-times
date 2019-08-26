49674ac888989c7275f9aebb18c278a27d002021
Ben Scott <bengscott2@gmail.com>
2019-08-26 14:33:02 +0100

    refactor: change tests to use new system

--- test/getTrainTimes.test.js
+++ test/getTrainTimes.test.js
@@ -1,34 +1,34 @@
 const getTrainTimes = require('../src/getTrainTimes');
 const MockAdapter = require('axios-mock-adapter');
-const axios = require('axios')
+const timesApiMock = require('../src/apiCalls').timesApi
 const dotenv = require('dotenv');
 dotenv.config()
 const APP_ID = process.env.APP_ID;
 const APP_KEY = process.env.APP_KEY;
-const mock = new MockAdapter(axios)
+const timesMock = new MockAdapter(timesApiMock)
 const expect = require('chai').expect
 const sinon = require('sinon')
 
-describe('#getTrainTimes', function () {
+describe('#getTrainTimes', async function () {
   it('calls API and returns data for given station', async function () {
     var apiUrl = `https://api.tfl.gov.uk/StopPoint/910GPCKHMQD/Arrivals?app_id=${APP_ID}&app_key=${APP_KEY}`
-    mock.onGet(apiUrl).reply(200, {
+    timesMock.onGet(apiUrl).reply(200, {
       test: [{data: 'test data'}]
     })
-    var station = {station: 'QueensRoadPeckham'}
+    var stationId = '910GPCKHMQD'
     var printerMock = sinon.spy()
-    result = await getTrainTimes(station, printerMock)
+    result = await getTrainTimes(stationId, printerMock)
     expect(result.test[0].data).to.equal('test data')
   })
 
   it('calls printer function', async function () {
     var apiUrl = `https://api.tfl.gov.uk/StopPoint/910GPCKHMQD/Arrivals?app_id=${APP_ID}&app_key=${APP_KEY}`
-    mock.onGet(apiUrl).reply(200, {
+    timesMock.onGet(apiUrl).reply(200, {
       test: [{data: 'test data'}]
     })
-    var station = {station: 'QueensRoadPeckham'}
+    var stationId = '910GPCKHMQD'
     var printerMock = sinon.spy()
-    result = await getTrainTimes(station, printerMock)
+    result = await getTrainTimes(stationId, printerMock)
     expect(printerMock.called).to.equal(true)
   })
 })
