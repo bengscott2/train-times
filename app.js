b4bd2b782c2eccaff2dd9a107df26f683ea66df8
Ben Scott <bengscott2@gmail.com>
2019-09-06 10:56:39 +0200

    refactor: remove ESM module, use pre ES6 across app for consistency

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const argsParser = require('./src/argsParser');
 const getTrainTimes = require('./src/getTrainTimes')
 const getStationId = require('./src/getStationId')
 
-export function trainTimes(args) {
+module.exports = function trainTimes(args) {
   console.log('Searching for train times..');
   getStationId(argsParser(args).station, getTrainTimes)
 }
